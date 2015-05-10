225b1e8ccdb207a1ed5d1cfd26eb3f6e51604b6d
Jaime <jpanaia@gmail.com>
2015-05-10 14:09:19 -0700

    Adoption project for PCS-AFE

---
+++ app/scripts/main.js
@@ -0,0 +1,16 @@
+// console.log('\'Allo \'Allo!');
+$.getJSON( "/ajax/kittens.json", function( data ) {
+  var items = [];
+
+  $.each( data, function( key, val ) {
+  	  $.each (val, function( index, item ) {
+       items.push( "<div class='media'><div class='media-left'>"  + "<img class='media-object' src='" + val[index].imageUrl + "'>" + "</div>" + "<div class='media-body'><h4 class='media-heading'>" + val[index].name + ": Age " + val[index].age + "</h4>" + val[index].description + "</div></div>" );   
+  });
+
+   $( "<div/>", {
+     	"class": "kittens",
+	     html: items.join( "" )
+	     }).appendTo( "#kittens" );
+	   });
+   
+});
